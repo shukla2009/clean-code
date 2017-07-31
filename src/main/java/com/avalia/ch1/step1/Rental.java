1ead683f98bb1843796816ea48c38d0af2fec2f9
Rahul <shukla2009@gmail.com>
2017-08-01 01:59:56 +0530

    first commit

---
+++ src/main/java/com/avalia/ch1/step1/Rental.java
@@ -0,0 +1,22 @@
+package com.avalia.ch1.step1;
+
+/**
+ * Created by rahul on 1/8/17.
+ */
+public class Rental {
+    private Movie _movie;
+    private int _daysRented;
+
+    public Rental(Movie movie, int daysRented) {
+        _movie = movie;
+        _daysRented = daysRented;
+    }
+
+    public int getDaysRented() {
+        return _daysRented;
+    }
+
+    public Movie getMovie() {
+        return _movie;
+    }
+}
